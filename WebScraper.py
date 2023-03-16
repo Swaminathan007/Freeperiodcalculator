5e1e49c8021195b86a28dceb0f8dbe0922de24f6
Swaminathan <swaminathang12b2svv@gmail.com>
2023-03-16 22:09:00 +0530

    added required files

---
+++ WebScraper.py
@@ -0,0 +1,100 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from PIL import Image
+import time
+op = Options()
+op.headless = True
+class Scraper:
+    def __init__(self):
+        self.path = "C:\Program Files (x86)\chromedriver.exe"
+        self.driver = webdriver.Chrome(self.path,options = op)
+        self.main_link = "https://webstream.sastra.edu/sastraparentweb/"
+        self.driver.get(self.main_link)
+        self.driver.maximize_window()
+        self.form = self.driver.find_element(By.XPATH,"//form[@id ='frmLogin']")
+        self.link = "https://webstream.sastra.edu/sastraparentweb/usermanager/home.jsp"
+    def getcaptcha(self):
+        img = self.form.find_element(By.ID,"imgCaptcha")
+        self.driver.save_screenshot("static/captcha.png")
+        location = img.location
+        location = img.location
+        size = img.size
+        ax = location['x']
+        ay = location['y']
+        width = location['x']+size['width']
+        height = location['y']+size['height']
+        cropImage = Image.open('static/captcha.png')
+        cropImage = cropImage.crop((int(ax), int(ay), int(width), int(height)))
+        cropImage.save('static/captcha1.png')
+    def login(self,regno,pwd,captcha):
+        number = self.form.find_element(By.ID,"txtRegNumber")
+        dob = self.form.find_element(By.ID,"txtPwd")
+        cap = self.form.find_element(By.ID,"answer")
+        number.send_keys(regno)
+        dob.send_keys(pwd)
+        cap.send_keys(captcha)
+        # time.sleep(10)
+        button = self.form.find_element(By.XPATH,"//input[@type = 'button']")
+        button.click()
+        try:
+            self.driver.get(self.link)
+            return True
+        except:
+            time.sleep(1)
+            return False
+    def get_details(self):
+        try:
+            print("logged in")
+            time.sleep(10)
+            form1 = self.driver.find_element(By.XPATH,"//form[@id ='frmStudentMain']")
+            link = self.driver.find_elements(By.TAG_NAME,"a")
+            for i in link:
+                if(i.text.lower() == "timetable"):
+                    i.click()
+                    break
+            time.sleep(5)
+            details = self.driver.find_elements(By.CLASS_NAME,"leftnavlinks01")
+            details = details[0].text.split("\n")
+            name = details[0]
+            course = details[1]
+            form2 = form1.find_element(By.XPATH,"//form[@id='frmStudentTimetable']")
+            table = form2.find_elements(By.CLASS_NAME,"tablecontent01")
+            section = table[68].text
+            return (name,course,section)
+        except:
+            return None
+    def get_timetable(self):
+        try:
+            link = self.driver.find_elements(By.TAG_NAME,"a")
+            for i in link:
+                if(i.text.lower() == "timetable"):
+                    i.click()
+                    break
+            time.sleep(5)
+            form2 = self.driver.find_element(By.XPATH,"//form[@id ='frmStudentTimetable']")
+            table = form2.find_elements(By.CLASS_NAME,"tablecontent01")
+            table = [i.text for i in table[:55]]
+            timetable = []
+            for i in range(0,55,11):
+                timetable.append(table[i:i+11])
+            for i in range(len(timetable)):
+                timetable[i] = timetable[i][:8]
+            return timetable
+        except:
+            return None
+    def logout(self):
+        link = self.driver.find_elements(By.TAG_NAME,"a")
+        for i in link:
+            if(i.text.lower() == "logout"):
+                i.click()
+                break
+        self.driver.get(self.main_link)
+        time.sleep(1)
+        self.form = self.driver.find_element(By.XPATH,"//form[@id ='frmLogin']")
+        
+# s = Scraper()
+# s.login("125003358","02012004","")
+# print(s.get_details())
---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import *
+from WebScraper import Scraper
+import time
+app = Flask(__name__)
+Details,Timetable,Name = [],[],[]
+count = 0
+signedin = False
+s = Scraper()
+countupdated = False
+app.config["SECRET_KEY"] = "1234"
+@app.route("/",methods = ["GET","POST"])
+def home():
+    global count
+    if(request.method == "POST"):
+        count = int(request.form.get("count"))
+        return redirect(url_for("getdetails",n=1))
+    return render_template("home.html")
+@app.route("/getdetails/<int:n>",methods = ["GET","POST"])
+def getdetails(n):
+    global Timetable,Name,Details,signedin,count
+    signedin = False
+    try:
+        s.getcaptcha()
+    except:
+        pass
+    if(not countupdated):
+        if(n > count):
+            return redirect(url_for("Frees"))
+    if(request.method == "POST"):
+        regno = request.form.get("regnumber")
+        pwd = request.form.get("pwd")
+        captcha = request.form.get("captcha")
+        signedin = True
+        if(s.login(regno,pwd,captcha)):
+            details = s.get_details()
+            timetable = s.get_timetable()
+            if details not in Details:
+                Details.append(details)
+                Timetable.append(timetable)
+                flash(details[0]+ " added successfully")
+            else:
+                flash("Person from same class added already")
+            s.logout()
+            return redirect(url_for("getdetails",n = n+1))
+        else:
+            flash("Incorrect regno or password")
+            return redirect(url_for("getdetails",n=n))
+    return render_template("login.html",number = n)
+@app.route("/Frees")
+def Frees():
+    timings = ["8:45-9:45","9:45-10:45","11:00-12:00","12:00-1:00","1:00-2:00","2:00-3:00","3:15-4:15","4:15-5:15"]
+    days = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+    freehrs = []
+    daysandhrs = []
+    for i in range(5):
+        for j in range(8):
+            daysandhrs.append([i,j])
+    for i in range(len(daysandhrs)):
+        c = 0
+        day = daysandhrs[i][0]
+        time = daysandhrs[i][1]
+        for j in range(len(Timetable)):
+            if(Timetable[j][day][time] == ''):
+                c+=1
+        if(c == count):
+            freehrs.append(daysandhrs[i])
+    return render_template("frees.html",Details=Details,timings=timings,days=days,Timetable=Timetable,freehrs=freehrs,n=count+1)
+@app.route("/updatecount")
+def updatecount():
+    global count,signedin,Details,countupdated
+    countupdated = True
+    count+=1
+    return redirect(url_for("getdetails",n=count))
+@app.route("/viewtimetable/<int:id>")
+def viewtimetable(id):
+    global Timetable,Details
+    timetable = Timetable[id]
+    details = Details[id]
+    timings = ["8:45-9:45","9:45-10:45","11:00-12:00","12:00-1:00","1:00-2:00","2:00-3:00","3:15-4:15","4:15-5:15"]
+    days = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+    return render_template("profile.html",timetable=timetable,details=details,timings=timings,days=days)
+@app.route("/delete/<int:id>")
+def deletestudent(id):
+    global Timetable,Details,count
+    if(count > 2):
+        Timetable.pop(id)
+        det = Details.pop(id)
+        count-=1
+        flash(det[0] + " deleted")
+    else:
+        flash("Minimum 2 students required")
+    return redirect(url_for("Frees"))
+app.run(debug=True,host="0.0.0.0")
+
